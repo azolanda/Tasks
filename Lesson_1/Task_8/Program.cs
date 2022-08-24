d374b5a22b387c803d158d9ede941389b641dacf
azolanda <uboikov@yandex.ru>
2022-08-24 11:03:58 +0300

    Task_8 added

---
+++ Lesson_1/Task_8/Program.cs
@@ -0,0 +1,20 @@
+﻿/*  Задача 8: Напишите программу, которая на вход принимает число (N), 
+    а на выходе показывает все чётные числа от 1 до N.   */
+
+    Console.WriteLine("Введите натуральное число: ");
+    int num = int.Parse(Console.ReadLine());
+
+    if(num < 1) Console.WriteLine("Введенное число не соответствует требованиям программы");
+    else
+    {
+        int count = 1;
+        while(count < num + 1)
+        {
+            if(count % 2 == 0)
+            {
+                Console.Write(count + " ");
+                count+= 2;
+            }
+            else count++;
+        }
+    }
