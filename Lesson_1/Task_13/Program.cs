b3b3e4f6098ca4a1a695b7d4a8f615a1e2482693
azolanda <uboikov@yandex.ru>
2022-08-29 13:55:33 +0300

    Task_13 added

---
+++ Lesson_1/Task_13/Program.cs
@@ -0,0 +1,38 @@
+﻿/*  Задача 13: Напишите программу, которая выводит третью цифру заданного числа или 
+    сообщает, что третьей цифры нет.  */
+
+int? ThirdNum(string str)
+{
+    int[] numArray = new int[3];
+
+    if (str[0] == '-')
+    {
+        str = str.Substring(1);
+    }
+
+    if (str.Length < 3)
+    {
+        return null;
+    }
+    else
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            numArray[i] = int.Parse(str[i].ToString());
+        }
+        int thirdNum = numArray[2];
+        return thirdNum;
+    }
+}
+
+Console.WriteLine("Введите трехзначное число:");
+string numStr = Console.ReadLine();
+var thirdNumber = ThirdNum(numStr);
+if (thirdNumber == null)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    Console.WriteLine("Третья цифра заданного числа: " + thirdNumber);
+}
