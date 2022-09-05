8cbc6b1fdc8e3e26f7676972a22707e5146e3997
azolanda <uboikov@yandex.ru>
2022-09-05 14:18:54 +0300

    Task_29 added

---
+++ Lesson_1/Task_29/Program.cs
@@ -0,0 +1,33 @@
+﻿/*  Задача 29: Напишите программу, которая задаёт массив из элементов 
+    и выводит их на экран.  */
+
+void PrintArray(string[] array)
+{
+    int length = array.Length;
+
+    string result = "[";
+    for (int i = 0; i < length; i++)
+    {
+        if (i == length - 1)
+        {
+            result += $"{array[i]}]";
+        }
+        else
+        {
+            result += $"{array[i]}, ";
+        }
+    }
+    Console.WriteLine($"Массив из введенных чисел: {result}");
+}
+
+Console.WriteLine("Введите элементы массива через пробел: ");
+string arrStr = Console.ReadLine();
+
+string[] arr = arrStr.Split(' ');
+
+PrintArray(arr);
+
+
+
+
+
