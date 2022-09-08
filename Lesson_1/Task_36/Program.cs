1d580b1e7fff1392c92dec82886c924ef6c15713
azolanda <uboikov@yandex.ru>
2022-09-08 07:52:26 +0300

    Task_36 added

---
+++ Lesson_1/Task_36/Program.cs
@@ -0,0 +1,68 @@
+﻿/*  Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+    Найдите сумму элементов, стоящих на нечётных позициях.  */
+
+int[] CreateArray(int len)
+{
+    int[] array = new int[len];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-10, 11);
+    }
+
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    int length = array.Length;
+
+    if (length == 0)
+    {
+        Console.WriteLine("Массив пустой");
+    }
+    else
+    {
+        string result = "[";
+        for (int i = 0; i < length; i++)
+        {
+            if (i == length - 1)
+            {
+                result += $"{array[i]}]";
+            }
+            else
+            {
+                result += $"{array[i]}, ";
+            }
+        }
+        Console.WriteLine($"Сгенерированный массив: {result}");
+    }
+}
+
+int SumOdd(int[] arr)
+{
+    int mySum = 0;
+
+    for (int i = 1; i < arr.Length; i += 2)
+    {
+        mySum += arr[i];
+    }
+
+    return mySum;
+}
+
+Console.WriteLine("Введите натуральное число - размер массива: ");
+int length = int.Parse(Console.ReadLine());
+
+if (length < 1)
+{
+    Console.WriteLine("Введено число в неверном формате. Введите натуральное число");
+}
+else
+{
+    int[] myArray = CreateArray(length);
+    int sum = SumOdd(myArray);
+
+    PrintArray(myArray);
+    Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях в сгенерированном массиве: {sum}");
+}
