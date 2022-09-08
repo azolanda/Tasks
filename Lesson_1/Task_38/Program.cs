a1d296036de02f50c2960a49683ee989276c5cff
azolanda <uboikov@yandex.ru>
2022-09-08 08:48:16 +0300

    Task_38 added

---
+++ Lesson_1/Task_38/Program.cs
@@ -0,0 +1,104 @@
+﻿/*  Задача 38: Задайте массив вещественных чисел. 
+    Найдите разницу между максимальным и минимальным элементов массива.  */
+
+double GetRandomNumber(double minimum, double maximum)
+{
+    double random = new Random().NextDouble();
+    return random * (maximum - minimum) + minimum;
+}
+
+double[] CreateArray(int len)
+{
+    double[] array = new double[len];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = GetRandomNumber(-100.00, 101.00);
+    }
+
+    return array;
+}
+
+void PrintArray(double[] array)
+{
+    int length = array.Length;
+
+    if (length == 0)
+    {
+        Console.WriteLine("Массив пустой");
+    }
+    else
+    {
+        string result = "[";
+        for (int i = 0; i < length; i++)
+        {
+            if (i == length - 1)
+            {
+                result += $"{Math.Round(array[i], 2)}]";
+            }
+            else
+            {
+                result += $"{Math.Round(array[i], 2)}, ";
+            }
+        }
+        Console.WriteLine($"Сгенерированный массив: {result}");
+    }
+}
+
+double Min(double[] arr)
+{
+    double min = arr[0];
+
+    for (int i = 1; i < arr.Length - 1; i++)
+    {
+        for (int j = 0; j < arr.Length - i; j++)
+        {
+            if (min > arr[i])
+            {
+                double temp = arr[i];
+                arr[i] = min;
+                min = temp;
+            }
+        }
+    }
+
+    return min;
+}
+
+double Max(double[] arr)
+{
+    double max = arr[0];
+
+    for (int i = 1; i < arr.Length - 1; i++)
+    {
+        for (int j = 0; j < arr.Length - i; j++)
+        {
+            if (max < arr[i])
+            {
+                double temp = arr[i];
+                arr[i] = max;
+                max = temp;
+            }
+        }
+    }
+
+    return max;
+}
+
+Console.WriteLine("Введите натуральное число - размер массива: ");
+int length = int.Parse(Console.ReadLine());
+
+if (length < 1)
+{
+    Console.WriteLine("Введено число в неверном формате. Введите натуральное число");
+}
+else
+{
+    double[] myArray = CreateArray(length);
+    double minimum = Min(myArray);
+    double maximum = Max(myArray);
+    double result = Math.Round(maximum - minimum, 2);
+
+    PrintArray(myArray);
+    Console.WriteLine($"Разница между максимальным ({Math.Round(maximum, 2)}) и минимальным ({Math.Round(minimum, 2)}) элементами в сгенерированном массиве: {result}");
+}
