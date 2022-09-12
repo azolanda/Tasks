015cb0f0e52c59c6a7fd8c3725735222214e8150
azolanda <uboikov@yandex.ru>
2022-09-12 16:34:23 +0300

    Task_41 added

---
+++ Lesson_1/Task_41/Program.cs
@@ -0,0 +1,26 @@
+﻿/*  Задача 41: Пользователь вводит с клавиатуры M чисел. 
+    Посчитайте, сколько чисел больше 0 ввёл пользователь. */
+
+int NumCounter(string[] array, int index)
+{
+    if (index < 0)
+    {
+        return 0;
+    }
+
+    int result = 0;
+
+    if (int.Parse(array[index]) > 0)
+    {
+        result = 1;
+    }
+
+    return result += NumCounter(array, index - 1);
+}
+
+Console.WriteLine("Введите набор целых чисел через запятую: ");
+string arrStr = Console.ReadLine();
+
+string[] arr = arrStr.Split(',');
+int count = NumCounter(arr, arr.Length - 1);
+Console.WriteLine(count);
