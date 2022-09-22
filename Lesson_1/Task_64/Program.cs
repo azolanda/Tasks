f1bde098aa651936e7e4b8962fa1f7505490e273
azolanda <uboikov@yandex.ru>
2022-09-22 11:06:49 +0300

    Task_64 added

---
+++ Lesson_1/Task_64/Program.cs
@@ -0,0 +1,21 @@
+﻿/*  Задача 64. Задайте значения M и N. Напишите программу, 
+    которая выведет все натуральные числа в промежутке от M до N.
+
+    M = 1; N = 5. -> "1, 2, 3, 4, 5"
+    M = 4; N = 8. -> "4, 5, 6, 7, 8"
+*/
+
+void PrintRecursiveNums(int m, int n)
+{
+    if (m == n)
+    {
+        Console.WriteLine($"{m}");
+    }
+    else
+    {
+        Console.Write($"{m}, ");
+        PrintRecursiveNums(++m, n);
+    }
+}
+
+PrintRecursiveNums(4, 8);
